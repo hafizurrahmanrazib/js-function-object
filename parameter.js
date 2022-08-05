4bae95f769ddb08c240b49761a92f05827086b04
hafizurrahmanrazib <hafizur201611044@gmail.com>
2022-08-05 17:22:20 +0600

    function parameter added

--- parameter.js
+++ parameter.js
@@ -1,5 +1,16 @@
 function bringSingara(money) {
     console.log('mama singara den');
-    console.log(money);
+    console.log('Taka Disen:', money);
 }
-bringSingara(100);
+var taka = 300;
+// bringSingara(taka);
+function add(num1, num2) {
+    console.log('Going to add:', num1, num2);
+}
+// add(125, 96);
+function sum(a, b, c, d, e) {
+    console.log(a, b, c, d, e);
+    var sum = a + b + c + d + e;
+    console.log(sum);
+}
+sum(15, 98, 56, 5, 9);
